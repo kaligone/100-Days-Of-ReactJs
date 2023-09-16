28a71a11a58d2bca7a5236ad680d6a74492c23d9
kaligone <61344302+kaligone@users.noreply.github.com>
2023-09-16 14:53:19 +0530

    Fifth Commit

--- Day5/index.js
+++ Day5/index.js
@@ -1,17 +1,10 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
 import './index.css'
-// import Netflix from './Netflix';
-// import Counter from './Counter';
-// import CurrentTime from './ProjectComponent/CurrentTime';
-import DarkMode from './ProjectComponent/DarkMode';
+import DarkMode from './DarkMode';
 
 ReactDOM.render(
     <>
-        {/* <Netflix/> */}
-        {/* <Counter/> */}
-        {/* <CurrentTime/> */}
-
         <DarkMode/>
     </>,
     document.getElementById('root')
